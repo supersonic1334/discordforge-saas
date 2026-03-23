0fb3f42191e38bbb2fac275f0213bd14a34162df
supersonic1334 <145559913+supersonic1334@users.noreply.github.com>
2026-03-23 20:27:40 +0100

    deploy: add backend/src/constants/languages.js

---
+++ backend/src/constants/languages.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const SITE_LANGUAGES = ['auto', 'fr', 'en', 'es'];
+const AI_LANGUAGES = ['auto', 'fr', 'en', 'es'];
+
+const LANGUAGE_LABELS = {
+  auto: 'Automatic',
+  fr: 'French',
+  en: 'English',
+  es: 'Spanish',
+};
+
+module.exports = {
+  SITE_LANGUAGES,
+  AI_LANGUAGES,
+  LANGUAGE_LABELS,
+};
